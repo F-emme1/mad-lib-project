4054f6b8bdcf7c73cb7a0bc6cb0899b13d46cb51
F-emme1 <engrum.emerald@gmail.com>
2020-02-07 17:36:32 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,33 @@
+print " Lets make a christmas story. Not just any kind of christmas story- your own unique one! "
+puts "Please enter information for the following: "
+puts "Name: "
+name = gets.chomp
+
+puts "Adverb: "
+adverb = gets.chomp
+
+puts "Plural Verb: "
+plural_verb = gets.chomp
+
+puts "Singular Noun: "
+singluar_noun = gets.chomp
+
+puts "Adjective: "
+adjective = gets.chomp
+
+puts "Verb: "
+verb = gets.chomp 
+
+puts "Liquid: "
+liquid = gets.chomp 
+
+puts "Plural Body Parts: "
+plural_body_parts = gets.chomp
+
+puts "Texture: "
+texture = gets.chomp
+
+
+puts " #{name} said 'im dreaming of a #{adverb}christmas.' "
+puts "The snow #{plural_verb} like 
+#{singluar_noun}! I love this holiday! We get to open #{adjective} presents with family, sitting next to the #{verb} fire. We drink #{liquid} while talking about the latest #{plural_body_parts}. I enjoy playing my  #{texture} instrument by the open fire. "
